fc2a1fc44f8f0ee454787b9e518ce57b79e16711
Erik Poole <Mahkumazahn@gmail.com>
2021-04-04 10:58:42 -0600

    multiple reservation times

--- README.md
+++ README.md
@@ -49,14 +49,13 @@ Modify the existing `reservations.json` file in this repo with the specific rest
 
 #### reservations
 
-**time**: The time of your desired reservation. Reservation times should be H:MM format in half hour increments (e.g. 7:30 is valid, 7:45 is not) or one of `Breakfast`, `Brunch`, `Lunch`, or `Dinner`. The checker is no smart enough to determine invalid times or entries. If you request `Brunch` at a restaurant that does not offer it you won't recieve results
+**times**: An array of times you're interested in a reservation for. Reservation times should be H:MM format in half hour increments (e.g. 7:30 is valid, 7:45 is not) or one of `Breakfast`, `Brunch`, `Lunch`, or `Dinner`. The checker is no smart enough to determine invalid times or entries. If you request `Brunch` at a restaurant that does not offer it you won't recieve results
 
 **date**: the date of your desired reservation in DD/MM/YY format (e.g. 01/04/2022)
 
 ## TODO
 * Handle instances where a reservation isn't available gracefully
 * Todos in code
-* Verify multiple reservations
 * Run on chron
 * Run in AWS instance
 * Configure user from file rather than env
--- main.py
+++ main.py
@@ -60,9 +60,9 @@ class Reservation:
     
     
     """
-    def __init__(self, time, date):
-        self.time = time
+    def __init__(self, date, times):
         self.date = date
+        self.times = times
 
 
 class Restaurant:
@@ -83,8 +83,38 @@ class Restaurant:
         self.reservations = reservations
         self.link = link
 
-def expand_shadow_element(driver, element):
-    return driver.execute_script('return arguments[0].shadowRoot', element)
+def main():
+    if EMAIL_USERNAME is None or EMAIL_PASSWORD is None or EMAIL_USERNAME is None or DISNEY_PASSWORD is None:
+        print("Missing required credentials in environment variables")
+        sys.exit(1)
+
+    infile = open("reservations.json", "r")
+    data = json.load(infile)
+    infile.close()
+
+    restaurants = []
+    for restaurant in data["restaurants"]:
+        name = restaurant["name"]
+        link = restaurant["link"]
+
+        reservations = []
+        for reservation in restaurant["reservations"]:
+            date = reservation["date"]
+
+            times = []
+            for time in reservation["times"]:
+                times.append(time)
+
+            reservations.append(Reservation(date, times))
+
+        restaurants.append(Restaurant(name, link, reservations))
+
+    driver = webdriver.Chrome()
+    login(driver)
+    alerts = get_availability(restaurants, driver)
+    driver.close()
+
+    send_alerts(alerts)
 
 def login(driver):
     driver.get(f'{BASE_URL}/login')
@@ -141,7 +171,7 @@ def get_availability(r_list, driver):
                 if months_diff < 0:
                     months_diff += 12
 
-                # todo break this into a validation step before selenium
+                # TODO (epoole) break this into a validation step before selenium
                 # reservations can be made only up to two months in advance
                 if months_diff > 2:
                     print(f'invalid reservation date requested - {reservation.date}; reservations can only be made up to 60 days in advance')
@@ -160,30 +190,36 @@ def get_availability(r_list, driver):
                 day_section = root.find_element_by_xpath(f'.//*[text()=" {day} "]')
                 day_section.click()
 
-                # # get time dropdown's #shadow-root
-                time_dropdown_wrapper = root.find_element_by_xpath('.//wdpr-single-select')
-                time_dropdown_root = expand_shadow_element(driver, time_dropdown_wrapper)
-                time_dropdown = time_dropdown_root.find_element_by_id('custom-dropdown-button')
-                time_dropdown.click()
-
-                # no easy way to wait for an element in #shadow-root to be visible
-                sleep(1) 
-                dropdown_elements = time_dropdown_root.find_elements_by_class_name('option-value-inner')
-                for dropdown_element in dropdown_elements:
-                    if reservation.time in dropdown_element.text:
-                        dropdown_element.click()
-                        break
-
-                search_button = root.find_element_by_xpath('.//finder-button')
-                search_button.click()
-
-                WebDriverWait(root, TIMEOUT).until(EC.presence_of_element_located((
-                    By.XPATH, f'.//*[text()="Reserve a table at"]')))
-
-                reservation_times = root.find_elements_by_xpath('.//*[@class="finder-button secondary ng-star-inserted"]')
                 times = []
-                for reservation_time in reservation_times:
-                    times.append(reservation_time.text)
+                for requested_time in reservation.times:       
+                    # get time dropdown's #shadow-root
+                    time_dropdown_wrapper = root.find_element_by_xpath('.//wdpr-single-select')
+                    time_dropdown_root = expand_shadow_element(driver, time_dropdown_wrapper)
+                    time_dropdown = time_dropdown_root.find_element_by_id('custom-dropdown-button')
+                    time_dropdown.click()
+
+                    # no easy way to wait for an element in #shadow-root to be visible
+                    # TODO (epoole) probably need to up sleep when running in remote
+                    sleep(1) 
+                    dropdown_elements = time_dropdown_root.find_elements_by_class_name('option-value-inner')
+                    for dropdown_element in dropdown_elements:
+                        if requested_time in dropdown_element.text:
+                            dropdown_element.click()
+                            break
+
+                    # search for reservations
+                    search_button = root.find_element_by_xpath('.//finder-button')
+                    search_button.click()
+
+                    # wait for reservation options
+                    WebDriverWait(root, TIMEOUT).until(EC.presence_of_element_located((
+                        By.XPATH, f'.//*[text()="Reserve a table at"]')))
+
+                    # add reservation options to results
+                    available_times = root.find_elements_by_xpath('.//*[@class="finder-button secondary ng-star-inserted"]')
+                    for available_time in available_times:
+                        times.append(available_time.text)
+
                 results.append(Alert(restaurant.name, reservation.date, times))
                 
         except:
@@ -191,6 +227,9 @@ def get_availability(r_list, driver):
             traceback.print_exc()
 
     return results
+
+def expand_shadow_element(driver, element):
+    return driver.execute_script('return arguments[0].shadowRoot', element)
 
 def send_alerts(alert_list):
     """A function for sending text alerts of Restaurants availability
@@ -210,54 +249,36 @@ def send_alerts(alert_list):
     
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+    message = ""
 
     for alert in alert_list:
-        message = f'\n{alert.restaurant_name} has reservations open for '
+        message += f'\n{alert.restaurant_name} has reservations open for '
 
         for time in alert.times:
             message += f'{time} '
         message += "on "
         message += alert.date
 
-        try:
-            server.sendmail(EMAIL_USERNAME, ["8014718540@vtext.com", "8016691177@vtext.com"], message)
-        except:
-            print("Error: unable to send:\n" + message)
+    try:
+        # server.sendmail(EMAIL_USERNAME, ["8014718540@vtext.com", "8016691177@vtext.com"], message)
+        print(message)
+    except:
+        print("Error: unable to send:\n" + message)
 
     server.quit()    
     print("Alerts Sent")
 
-def main():
-    if EMAIL_USERNAME is None or EMAIL_PASSWORD is None or EMAIL_USERNAME is None or DISNEY_PASSWORD is None:
-        print("Missing required credentials in environment variables")
-        sys.exit(1)
-
-    infile = open("reservations.json", "r")
-    data = json.load(infile)
-    infile.close()
-
-    restaurant_list = []
-    # parse data and convert to objects
-    for x in data["places"]:
-        name = x["name"]
-        link = x["link"]
-        # stores temp list of reservations
-        reservation_list = []
-
-        for y in x["reservations"]:
-            time = y["time"]
-            date = y["date"]
-            res = Reservation(time, date)
-            reservation_list.append(res)
-
-        restaurant_list.append(Restaurant(name, link, reservation_list ))
-
-    driver = webdriver.Chrome()
-    login(driver)
-    alerts = get_availability(restaurant_list, driver)
-    driver.close()
-
-    send_alerts(alerts)
-
 if __name__ == "__main__":
     main()
+
+		# {
+		# 	"name": "Rainforest Cafe",
+		# 	"link": "https://disneyworld.disney.go.com/dining/disney-springs/rainforest-cafe-disney-springs/availability-modal",
+
+		# 	"reservations": [
+		# 		{
+		# 			"time": "Dinner",
+		# 			"date": "05/08/21"
+		# 		}
+		# 	]
+		# }
